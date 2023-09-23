770abdb5cd2497a632f48e1c43988d4db2ec90cd
Aloysius <aloysiusng5@gmail.com>
2023-09-24 01:28:52 +0800

    replace hyphen

--- terraform/create_attach_iam_policies/main.tf
+++ terraform/create_attach_iam_policies/main.tf
@@ -27,7 +27,7 @@ resource "aws_iam_policy" "attached_policies" {
 
 resource "aws_iam_policy_attachment" "attachment" {
   count      = length(var.policy_names)
-  name       = "${var.policy_names[count.index]}-attachment"
+  name       = "${var.policy_names[count.index]}_attachment"
   roles      = var.lambda_role_arns
   policy_arn = aws_iam_policy.attached_policies[count.index].arn
 }
