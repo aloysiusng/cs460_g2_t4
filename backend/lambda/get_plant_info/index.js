db99776979f63953e612fb09268fb3b1312ad4ab
Aloysius <aloysiusng5@gmail.com>
2023-10-02 17:07:31 +0800

    update code to working

--- backend/lambda/get_plant_info/index.js
+++ backend/lambda/get_plant_info/index.js
@@ -1,44 +1,78 @@
 const AWS = require("aws-sdk");
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-/*
-  # hash key -> :PK
-  hash_key = "plant_id"
-  attribute {
-    name = "plant_id"
-    type = "S"
-  }
-  # sort key -> :SK
-  range_key = "timestamp"
-  attribute {
-    name = "timestamp"
-    type = "S"
-  }
-*/
+
 exports.handler = async (event, context) => {
-  // get plant_id from event (query params)
+  // check if query string parameters are valid
+  if (event.queryStringParameters == null || event.queryStringParameters.plant_id == null) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Missing query string parameters",
+      }),
+    };
+  }
   const plant_id = event.queryStringParameters.plant_id;
+  const before_timestamp = event.queryStringParameters.before_timestamp;
+  const after_timestamp = event.queryStringParameters.after_timestamp;
+
+  if (before_timestamp > after_timestamp) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "before_timestamp must be before after_timestamp",
+      }),
+    };
+  }
+  // prepare dydb query params
+  var { KeyConditionExpression, ExpressionAttributeNames, ExpressionAttributeValues } = prepareParamsHelper(plant_id, before_timestamp, after_timestamp);
 
   // get plant info from dynamodb
   const params = {
     TableName: process.env.TABLE_NAME,
-    KeyConditionExpression: "#plant_id = :plant_id",
-    ExpressionAttributeNames: {
-      "#plant_id": "plant_id",
-    },
-    ExpressionAttributeValues: {
-      ":plant_id": plant_id,
-    },
+    KeyConditionExpression: KeyConditionExpression,
+    ExpressionAttributeNames: ExpressionAttributeNames,
+    ExpressionAttributeValues: ExpressionAttributeValues,
   };
   const data = await dynamoDb.query(params).promise();
-  console.log(data);
 
   // return plant info
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "get_plant_info",
-      data: data,
+      message: "Get plant info",
+      data: data.Items,
     }),
   };
 };
+
+// helper function to prepare dydb query params
+function prepareParamsHelper(plant_id, before_timestamp, after_timestamp) {
+  var KeyConditionExpression = "#P = :plant_id";
+  var ExpressionAttributeNames = {
+    "#P": "plant_id",
+  };
+  var ExpressionAttributeValues = {
+    ":plant_id": plant_id,
+  };
+
+  if (before_timestamp != null && after_timestamp != null) {
+    // both => between the 2 timestamps
+    KeyConditionExpression += " AND #T BETWEEN :before_timestamp AND :after_timestamp";
+    ExpressionAttributeValues[":before_timestamp"] = before_timestamp;
+    ExpressionAttributeValues[":after_timestamp"] = after_timestamp;
+  } else if (before_timestamp == null && after_timestamp != null) {
+    // only after timestamp
+    KeyConditionExpression += " AND #T >= :after_timestamp";
+    ExpressionAttributeValues[":after_timestamp"] = after_timestamp;
+  } else if (after_timestamp != null && after_timestamp == null) {
+    // only before timestamp
+    KeyConditionExpression += " AND #T <= :before_timestamp";
+    ExpressionAttributeValues[":before_timestamp"] = before_timestamp;
+  }
+  if (ExpressionAttributeValues[":before_timestamp"] != null || ExpressionAttributeValues[":after_timestamp"] != null) {
+    ExpressionAttributeNames["#T"] = "timestamp";
+  }
+
+  return { KeyConditionExpression, ExpressionAttributeNames, ExpressionAttributeValues };
+}
