e394f67ed49a586a97e2771ed41b19b03bb8a5ae
Aloysius <aloysiusng5@gmail.com>
2023-10-02 13:33:11 +0800

    try../../

--- terraform/lambda_and_apigw/data.tf
+++ terraform/lambda_and_apigw/data.tf
@@ -1,5 +1,5 @@
 data "archive_file" "lambda_function_zip" {
   type        = "zip"
   source_dir  = var.path_to_lambda_dir
-  output_path = "../backend/lambda/${var.lambda_function_name}.zip"
+  output_path = "../../backend/lambda/${var.lambda_function_name}.zip"
 }
