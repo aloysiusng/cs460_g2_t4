7d7ee3e27ef362d69796834c8ddaf0c96abe0fd5
Faisal Samudra <isalsamudra@hotmail.com>
2023-10-14 21:28:06 +0800

    bug fixes from get_threshold

--- backend/lambda/get_threshold/index.js
+++ backend/lambda/get_threshold/index.js
@@ -3,63 +3,60 @@ const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context) => {
-  // check if query string parameters are valid
-  if (
-    event.queryStringParameters == null ||
-    event.queryStringParameters.plant_id == null
-  ) {
+  try {
+    // Check if query string parameters are valid
+    if (
+      event.queryStringParameters == null ||
+      event.queryStringParameters.plant_id == null
+    ) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Missing query string parameters",
+        }),
+      };
+    }
+
+    const plant_id = event.queryStringParameters.plant_id;
+
+    // Prepare DynamoDB query parameters
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      KeyConditionExpression: "#P = :plant_id",
+      ExpressionAttributeNames: {
+        "#P": "plant_id",
+      },
+      ExpressionAttributeValues: {
+        ":plant_id": plant_id,
+      },
+    };
+
+    const data = await dynamoDb.query(params).promise();
+
+    if (data.Items.length === 0) {
+      return {
+        statusCode: 404, // Not Found
+        body: JSON.stringify({
+          message: "No threshold data found for the provided plant_id.",
+        }),
+      };
+    }
+
+    // Return plant info
     return {
-      statusCode: 400,
+      statusCode: 200,
       body: JSON.stringify({
-        message: "Missing query string parameters",
+        message: "Get Threshold Data",
+        data: data.Items,
+      }),
+    };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      statusCode: 500, // Internal Server Error
+      body: JSON.stringify({
+        message: "Internal Server Error",
       }),
     };
   }
-
-  const plant_id = event.queryStringParameters.plant_id;
-
-  // get plant info from dynamodb
-  const params = {
-    TableName: process.env.TABLE_NAME,
-    KeyConditionExpression: KeyConditionExpression,
-    ExpressionAttributeNames: ExpressionAttributeNames,
-    ExpressionAttributeValues: ExpressionAttributeValues,
-  };
-
-  // prepare dydb query params
-  var {
-    KeyConditionExpression,
-    ExpressionAttributeNames,
-    ExpressionAttributeValues,
-  } = prepareParamsHelper(plant_id, before_timestamp, after_timestamp);
-
-  const data = await dynamoDb.query(params).promise();
-  const output = data.Items
-
-  // return plant info
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Get Threshold Data",
-      output
-    }),
-  };
 };
-
-// helper function to prepare dydb query params
-function prepareParamsHelper(plant_id, before_timestamp, after_timestamp) {
-  var KeyConditionExpression = "#P = :plant_id";
-  var ExpressionAttributeNames = {
-    "#P": "plant_id",
-  };
-
-  var ExpressionAttributeValues = {
-    ":plant_id": plant_id,
-  };
-
-  return {
-    KeyConditionExpression,
-    ExpressionAttributeNames,
-    ExpressionAttributeValues,
-  };
-}
