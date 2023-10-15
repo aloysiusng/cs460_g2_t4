ee2581a3306371d30daaa571c14ec4178e6c8d65
dtjww <97948483+dtjww@users.noreply.github.com>
2023-10-15 16:51:56 +0800

    updated to handle empty data

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -2,28 +2,34 @@
     <v-container style="max-width: 1280px;">
         <h1 class="heading-1 font-weight-medium text-center">Welcome back to Plantify!</h1>
     </v-container>
-    <v-container>
+    
+    <v-container v-if="loading">
+        <v-row>
+            <v-col cols="8" md="8" order-md="1">
+                <v-skeleton-loader type="image" :loading="loading" v-if="loading">
+                </v-skeleton-loader>
+                <v-skeleton-loader type="image" :loading="loading" v-if="loading">
+                </v-skeleton-loader>
+            </v-col>
+            <v-col cols="4" md="4" order-md="2">
+                <v-skeleton-loader type="table-heading" :loading="loading" v-if="loading">
+                </v-skeleton-loader>
+            </v-col>
+        </v-row>
+    </v-container>
+    <v-container v-if="plantData">
         <v-row>
             <v-col cols="12" md="4" order-md="2">
-                <v-skeleton-loader type="table" :loading="loading" v-if="loading"></v-skeleton-loader>
                 <v-container>
                     <Summary />
                 </v-container>
             </v-col>
             <v-col cols="12" md="8" order-md="1">
-                <v-skeleton-loader type="image" :loading="loading" v-if="loading">
-                </v-skeleton-loader>
-
                 <v-container>
                     <v-card elevation="2" class="mb-2" v-if="!loading">
                         <ChartTemp :plantData="this.plantData" />
                     </v-card>
                 </v-container>
-
-
-                <v-skeleton-loader type="image" :loading="loading" v-if="loading">
-                </v-skeleton-loader >
-
                 <v-container>
                     <v-card elevation="2" class="mb-2" v-if="!loading">
                         <ChartWaterLevel :plantData="this.plantData" />
@@ -31,6 +37,17 @@
                 </v-container>
             </v-col>
         </v-row>
+    </v-container>
+    <v-container v-if=" loading == false && plantData == null ">
+        <v-card>
+            <v-card-text>
+                <v-row>
+                    <v-col cols="12">
+                        <p class="text-h6 font-weight-medium text-center"> No data available </p>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
     </v-container>
 </template>
 
