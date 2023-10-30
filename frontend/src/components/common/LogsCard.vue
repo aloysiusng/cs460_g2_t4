9383e84ee942b703d7bb401e31e399fc58aea5ba
dtjww <97948483+dtjww@users.noreply.github.com>
2023-10-30 19:14:41 +0800

    logcard in progress

--- frontend/src/components/common/LogsCard.vue
+++ frontend/src/components/common/LogsCard.vue
@@ -1,48 +1,65 @@
 <template>
-    <v-card>
-        <v-data-table>
-            <thead>
-                <tr>
-                <th class="text-left">
-                    time_stamp
-                </th>
-                <th class="text-left">
-                    Humidity Level
-                </th>
-                <th class="text-left">
-                    Moisture Level
-                </th>
-                <th class="text-left">
-                    Water Level
-                </th>
-                <th class="text-left">
-                    Sunlight Level
-                </th>
-                </tr>
-            </thead>
-            <tbody>
+  <!-- {{ plantData  }} -->
+    <v-card v-if="plantData && !loading">
+        <v-data-table
+            v-model:items-per-page="itemsPerPage"
+            :headers="headers"
+            :items="data"
+            item-value="time_stamp"
+            >
+            <template v-slot:item="{ item }">
+      <tr>
+        <td>{{ item.time_stamp }}</td>
+        <td>{{ item.humidity_level }}</td>
+        <td>{{ item.moisture_level }}</td>
+        <td>{{ item.water_level }}</td>
+        <td>{{ item.sunlight_level }}</td>
+      </tr>
+    </template>
+            <!-- <tbody>
                 <tr
                 v-for="plant in plantData"
                 :key="plant.plant_id"
                 >
                 <td>{{ plant.time_stamp }}</td>
-                <td>{{ plant.humidity_level }} <v-progress-linear model-value=plant.humidity_level height="8" color="blue" rounded></v-progress-linear></td>
+                <td>{{ plant.humidity_level }}</td>
                 <td>{{ plant.moisture_level }}</td>
                 <td>{{ plant.water_level }}</td>
                 <td>{{ plant.sunlight_level }}</td>
                 </tr>
-            </tbody>
+            </tbody> -->
         </v-data-table>
     </v-card>
   </template>
 
   <script>
+  import { VDataTableServer } from 'vuetify/labs/VDataTable'
     export default {
         props: {
-            plantData: Object
+            plantData: Array
         }, 
         data () {
         return {
+          components: {
+            VDataTableServer
+          },
+          data: [
+          { sunlight_level: 0, time_stamp: "10/15/2023, 05:18:17 PM", water_level: 0, humidity_level: null, moisture_level: 1 },
+          ],
+          loading: true,
+          itemsPerPage: 10,
+          headers: [
+            {
+              title: "Time Stamp",
+              align: "left",
+              sortable: true,
+              key: "time_stamp",
+            },  
+            { title: "Humidity Level", key: "humidity_level" },
+            { title: "Moisture Level", key: "moisture_level" },
+            { title: "Water Level", key: "water_level" },
+            { title: "Sunlight Level", key: "sunlight_level" },
+          ],
           options: {
             year: "numeric",
             month: "2-digit",
@@ -56,19 +73,16 @@
       mounted(){
         console.log(this.plantData)
         if (this.plantData) {
-          this.formatData(this.plantData)
+          this.loading = false
+          this.formatTimestamp(this.plantData)
         }
       },
       methods: {
-        formatData(plantData) {
+        formatTimestamp(plantData) {
           for (var data of plantData) {
             data.time_stamp = new Date(data.time_stamp).toLocaleString("en-US", this.options)
           }
-        }
-        // formatData(plantData) {
-        //   for (var data of plantData) {
-        //     data.time_stamp = new Date(data.time_stamp).toISOString()
-        //     data.time_stamp = data.time_stamp.slice(0, 10) + " " + data.time_stamp.slice(11, 19)
-          }
-        }
+        },
+      }
+    }
   </script>
