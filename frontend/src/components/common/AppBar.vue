4cceae01876b483f42636f9d03afa1300f814ed7
absolutelynoot <isalsamudra@hotmail.com>
2023-11-07 19:33:01 +0800

    hide logs

--- frontend/src/components/common/AppBar.vue
+++ frontend/src/components/common/AppBar.vue
@@ -74,10 +74,6 @@ export default {
                 value: 'dashboard',
             },
             {
-                title: 'Logs',
-                value: 'logs',
-            },
-            {
                 title: 'Logout',
                 value: 'logout',
             },
